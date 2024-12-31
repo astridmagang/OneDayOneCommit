3f820fc426a0506fc11f865ddc79131405719b6b
Astrid <astridmagang@gmail.com>
2024-12-31 21:36:41 +0700

    Json naming convention : SnakeCase

---
+++ Latihan1/CaseNamingPolicy.Test/Json/SnakeCaseNamingPolicyTest.cs
@@ -0,0 +1,22 @@
+﻿using CaseNamingPolicy.Moduls.Json;
+using FluentAssertions;
+
+namespace CaseNamingPolicy.Test.Json
+{
+    public class SnakeCaseNamingPolicyTest
+    {
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("A", "a")]
+        [InlineData("Aa", "aa")]
+        [InlineData("AA", "a_a")]
+        [InlineData("AAa", "a_aa")]
+        [InlineData("AaA", "aa_a")]
+        public void CanConvertPascalCaseToSnakeCase (string pascalCase, string expectedSnakeCase)
+        {
+            string snakeCase = new SnakeCaseNamingPolicy().ConvertName(pascalCase);
+
+            snakeCase.Should().Be(expectedSnakeCase);
+        }
+    }
+}
---
+++ Latihan1/CaseNamingPolicy/Moduls/Json/SnakeCaseNamingPolicy.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace CaseNamingPolicy.Moduls.Json
+{
+    public class SnakeCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            if (name == "" || name == null) return "";
+
+            Span<char> nameSpan = stackalloc char[name.Length + name.Skip(1).Count(char
+                .IsUpper)];
+
+            int i = 0;
+
+            nameSpan[i++] = char.ToLower(name[0]);
+
+            foreach (char c in name.Skip(1))
+            {
+                if (char.IsUpper(c))
+                {
+                    nameSpan[i++] = '_';
+                    nameSpan[i++] = char.ToLower(c);
+                }
+                else
+                {
+                    nameSpan[i++] = c;
+                }
+            }
+
+            return new string(nameSpan);
+        }
+    }
+}
