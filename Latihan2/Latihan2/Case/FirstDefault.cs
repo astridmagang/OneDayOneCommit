16ef305a5b7cbfdac73bf3cf11aaed631ee4d8d3
Astrid <astridmagang@gmail.com>
2025-01-01 22:09:07 +0700

    Exercise 2. Benchmark first or default ef.

---
+++ Latihan2/Latihan2/Case/FirstDefault.cs
@@ -0,0 +1,42 @@
+﻿namespace Latihan2.Case
+{
+    public class FirstDefaultCase
+    {
+        private readonly DataProduct _product;
+
+        public FirstDefaultCase(DataProduct product)
+        {
+            _product = product;
+        }
+
+        public DataProduct.Product GetProduct_Equals_PlusWhere (string namaProduct)
+        {
+            return _product.GetProducts().Where(a => a.ProductName.Equals(namaProduct)).FirstOrDefault() ?? new DataProduct.Product();
+        }
+
+        public DataProduct.Product GetProduct_Equals_NoWhere(string namaProduct)
+        {
+            return _product.GetProducts().FirstOrDefault(a => a.ProductName.Equals(namaProduct)) ?? new DataProduct.Product();
+        }
+
+        public DataProduct.Product GetProduct_EqualsWithStringComparison_PlusWhere(string namaProduct)
+        {
+            return _product.GetProducts().Where(a => a.ProductName.Equals(namaProduct, StringComparison.OrdinalIgnoreCase)).FirstOrDefault() ?? new DataProduct.Product();
+        }
+
+        public DataProduct.Product GetProduct_EqualsWithStringComparison_NoWhere(string namaProduct)
+        {
+            return _product.GetProducts().FirstOrDefault(a => a.ProductName.Equals(namaProduct, StringComparison.OrdinalIgnoreCase)) ?? new DataProduct.Product();
+        }
+
+        public DataProduct.Product GetProduct_AssignementOperator_PlusWhere (string namaProduct)
+        {
+            return _product.GetProducts().Where(a => a.ProductName == namaProduct).FirstOrDefault() ?? new DataProduct.Product();
+        }
+
+        public DataProduct.Product GetProduct_AssignementOperator_NoWhere(string namaProduct)
+        {
+            return _product.GetProducts().FirstOrDefault(a => a.ProductName == namaProduct) ?? new DataProduct.Product();
+        }
+    }
+}
